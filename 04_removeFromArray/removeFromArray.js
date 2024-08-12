50d1a752ba5d8ead9e9ff38116297a93358dfcc8
Carol Wang <110089439+ycarolwang@users.noreply.github.com>
2024-08-13 00:05:25 +0800

    finished first 7 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  // method 1:
+  //   for (const arg of args) {
+  //     for (let i = 0; i < array.length; i++) {
+  //       if (arg === array[i]) {
+  //         array.splice(i, 1);
+  //         i--;
+  //       }
+  //     }
+  //   }
+  //   return array;
+
+  // method 2:
+  //   const new_array = [];
+  //   for (const item of array) {
+  //     if (!args.includes(item)) {
+  //       new_array.push(item);
+  //     }
+  //   }
+  //   return new_array;
+
+  // method 3:
+  return array.filter((val) => !args.includes(val));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let sum;
+  if (
+    start < 0 ||
+    end < 0 ||
+    Math.floor(start) != start ||
+    Math.floor(end) != end ||
+    typeof start != "number" ||
+    typeof end != "number"
+  ) {
+    return "ERROR";
+  }
 
+  if (end >= start) {
+    sum = ((start + end) * (end - start + 1)) / 2;
+  } else {
+    sum = ((start + end) * (start - end + 1)) / 2;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
